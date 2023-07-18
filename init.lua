27771e4966450a2981e552bd3727165f5b6e008c
Valikoz <valikoz1202@gmail.com>
2023-07-18 10:54:53 +0300

    add luasnip config

--- init.lua
+++ init.lua
@@ -1,4 +1,18 @@
 return {
   -- Set colorscheme to use
   colorscheme = "astrodark",
+  
+  plugins = {
+    {
+      "L3MON4D3/LuaSnip",
+      config = function(plugin, opts)
+        -- include the default astronvim config that calls the setup call
+        require "plugins.configs.luasnip"(plugin, opts)
+        -- load snippets paths
+        require("luasnip.loaders.from_lua").lazy_load {
+          paths = { "./lua/user/snippets" }
+        }
+      end,
+    },
+  },
 }
