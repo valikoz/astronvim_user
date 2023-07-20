4cc33999f8bce4fc18242e32854052e391b3165e
Valikoz <valikoz1202@gmail.com>
2023-07-20 20:40:59 +0300

    update init.lua
    add new commands

--- init.lua
+++ init.lua
@@ -11,6 +11,15 @@ return {
 
   autocmds = {
     -- Remove whitespace
-    vim.api.nvim_create_user_command('WhiteSpace', '%s/\\s\\+$//e|nohlsearch', {})
+    vim.api.nvim_create_user_command('WhiteSpace', '%s/\\s\\+$//e|nohlsearch', {}),
+    -- Edit snippets
+    vim.api.nvim_create_user_command("LuaSnipEdit", function()
+      require('luasnip.loaders').edit_snippet_files()
+    end, { bang = true, desc = "Edit snippets" }),
+    -- Open snippets list
+    vim.api.nvim_create_user_command("LuaSnipInfo", function()
+      require("luasnip.extras.snippet_list").open()
+    end, { bang = true, desc = "Open snippets list" })
   },
+  
 }
