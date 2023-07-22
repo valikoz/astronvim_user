f0d4994cb0b4da2eb621a5148e76f55ae0eb5c32
Valikoz <valikoz1202@gmail.com>
2023-07-22 18:55:25 +0300

    update init.lua: add new globals etc.

--- init.lua
+++ init.lua
@@ -7,19 +7,23 @@ return {
       wrap = true, -- sets vim.opt.wrap
       swapfile = false,
     },
+    g = {
+      python3_host_prog = '~/AppData/Local/nvim/lua/user/env/Scripts/python',
+      loaded_ruby_provider = 0,
+      loaded_perl_provider = 0,
+    },
   },
 
   autocmds = {
     -- Remove whitespace
-    vim.api.nvim_create_user_command('WhiteSpace', '%s/\\s\\+$//e|nohlsearch', {}),
+    vim.api.nvim_create_user_command('WhiteSpaceClean', '%s/\\s\\+$//e|nohlsearch', {}),
     -- Edit snippets
-    vim.api.nvim_create_user_command("LuaSnipEdit", function()
-      require('luasnip.loaders').edit_snippet_files()
-    end, { bang = true, desc = "Edit snippets" }),
+    -- vim.api.nvim_create_user_command("LuaSnipEdit", function()
+    --   require('luasnip.loaders').edit_snippet_files()
+    -- end, { bang = true, desc = "Edit snippets" }),
     -- Open snippets list
-    vim.api.nvim_create_user_command("LuaSnipInfo", function()
-      require("luasnip.extras.snippet_list").open()
-    end, { bang = true, desc = "Open snippets list" })
+    -- vim.api.nvim_create_user_command("LuaSnipInfo", function()
+    --   require("luasnip.extras.snippet_list").open()
+    -- end, { bang = true, desc = "Open snippets list" })
   },
-  
 }
