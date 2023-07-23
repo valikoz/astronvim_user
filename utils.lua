88293185031a62073c11d443cede2195cbc7c2e7
Valikoz <valikoz1202@gmail.com>
2023-07-23 15:30:42 +0300

    update utils.lua

--- utils.lua
+++ utils.lua
@@ -1,19 +1,19 @@
 local M = {}
 
----@param {cmd} table
----@param {opts} is a table with these keys:
+---@param cmd table
+---@param opts table is a table with these keys:
   -- title: (string or nil) Notification's title.
   -- on_finish: (function, default=nil) Attach to on_exit.
 function M.async_run(cmd, opts)
 
   local function on_event(_, data, event)
     if #data ~= 1 then
-      vim.schedule(function() 
-        vim.notify(table.concat(data):gsub("\r", "\n"), 
+      vim.schedule(function()
+        vim.notify(table.concat(data):gsub("\r", "\n"),
           -- Log level 2 -> INFO, 4 -> ERROR.
           (event == "stdout" and 2) or 4,
           {title = opts.title}
-        ) 
+        )
       end)
     end
   end
