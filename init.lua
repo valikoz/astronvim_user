a1bc1dfbf4bab8094d0603bac298480714d29bd4
Valikoz <valikoz1202@gmail.com>
2023-07-24 10:11:48 +0300

    update init.lua

--- init.lua
+++ init.lua
@@ -19,7 +19,7 @@ return {
     vim.api.nvim_create_user_command('WhiteSpaceClean', '%s/\\s\\+$//e|nohlsearch', {}),
     -- Edit snippets
     vim.api.nvim_create_user_command("LuaSnipEdit", function()
-      require('luasnip.loaders').edit_snippet_files()
+      require('luasnip.loaders').edit_snippet_files({})
     end, { bang = true, desc = "Edit snippets" }),
     -- Open snippets list
     vim.api.nvim_create_user_command("LuaSnipInfo", function()
