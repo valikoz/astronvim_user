a9c5dd507af1791edfb4cc294e35ec7d19c3f564
Valikoz <valikoz1202@gmail.com>
2023-07-24 10:12:31 +0300

    update mappings.lua

--- mappings.lua
+++ mappings.lua
@@ -1,17 +1,31 @@
 local utils = require "user.utils"
 return {
-  n = { 
+  n = {
     ["<leader>mp"] = {
       function()
+        local filename = vim.fn.expand "%:t"
         if vim.bo.filetype == "python" then
           vim.cmd "silent! write"
-          local filename = vim.fn.expand "%:t"
           utils.async_run({ "python", vim.fn.expand "%:p" }, { title = "Execute " .. filename })
         else
+          vim.notify("Cannot execute " .. filename, 3, { title = "Warning" } )
           return nil
         end
       end,
       desc = "Execute python file",
+    },
+    ["<leader>ml"] = {
+      function()
+        local filename = vim.fn.expand "%:t"
+        if vim.bo.filetype == "lua" then
+          vim.cmd "silent! write"
+          utils.async_run({ "lua", vim.fn.expand "%:p" }, { title = "Execute " .. filename })
+        else
+          vim.notify("Cannot execute " .. filename, 3, { title = "Warning" } )
+          return nil
+        end
+      end,
+      desc = "Execute lua file",
     },
   },
 }
