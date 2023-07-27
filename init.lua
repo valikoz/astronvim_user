188bfb3fb26b5896d80cf96a3307f2c6fffa1c6c
Valikoz <valikoz1202@gmail.com>
2023-07-27 16:08:38 +0300

    update

--- init.lua
+++ init.lua
@@ -1,16 +1,16 @@
+local utils = require "user.utils"
 return {
-  -- Set colorscheme to use
-  colorscheme = "astrodark",
-
   options = {
     opt = {
       wrap = true, -- sets vim.opt.wrap
       swapfile = false,
+      cb = vim.fn.has "unix" == 1 and "" or "unnamedplus"
     },
     g = {
       -- mapleader = ",",
-      python3_host_prog = vim.fn.has "win64" == 1 and
-          '~/AppData/Local/nvim/lua/user/env/Scripts/python' or nil,
+      python3_host_prog = vim.fn.has "win64" == 1
+        and '~/AppData/Local/nvim/lua/user/env/Scripts/python' 
+        or '~/.config/nvim/lua/user/env/bin/python',
       loaded_ruby_provider = 0,
       loaded_perl_provider = 0,
     },
@@ -26,6 +26,52 @@ return {
     -- Open snippets list
     vim.api.nvim_create_user_command("LuaSnipInfo", function()
       require("luasnip.extras.snippet_list").open()
-    end, { bang = true, desc = "Open snippets list" })
+    end, { bang = true, desc = "Open snippets list" }),
+  },
+
+  mappings = {
+    n = {
+        -- disable default bindings
+      ["<C-Down>"] = false,
+      ["<C-Left>"] = false,
+      ["<C-Right>"] = false,
+      ["<C-Up>"] = false,
+      ["<C-q>"] = false,
+      ["<C-s>"] = false,
+      ["<leader>mp"] = {
+        function()
+          local filename = vim.fn.expand "%:t"
+          if vim.bo.filetype == "python" then
+            vim.cmd "silent! write"
+            utils.async_run({ "python3", vim.fn.expand "%:p" }, {
+              title = "Execute " .. filename 
+            })
+          else
+            vim.notify("Cannot execute " .. filename, 3, { title = "Warning" } )
+            return nil
+          end
+        end,
+        desc = "Execute python file",
+      },
+      ["<leader>ml"] = {
+        function()
+          local filename = vim.fn.expand "%:t"
+          if vim.bo.filetype == "lua" then
+            vim.cmd "silent! write"
+            utils.async_run({ "lua", vim.fn.expand "%:p" }, { title = "Execute " .. filename })
+          else
+            vim.notify("Cannot execute " .. filename, 3, { title = "Warning" } )
+            return nil
+          end
+        end,
+        desc = "Execute lua file",
+      },
+    },
+  },
+  
+  lsp = {
+    servers = {
+      "pyright",
+    },
   },
 }
