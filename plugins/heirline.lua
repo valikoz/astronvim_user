85ba04a388fed514dadfe5a11a2fac3692e3a61d
Valikoz <valikoz1202@gmail.com>
2023-07-29 18:38:45 +0300

    customize plugin

--- plugins/heirline.lua
+++ plugins/heirline.lua
@@ -1,27 +1,10 @@
 return {
   "rebelot/heirline.nvim",
   opts = function(_, opts)
-    local status = require("astronvim.utils.status")
-    opts.statusline = { -- statusline
-      hl = { fg = "fg", bg = "bg" },
-      status.component.mode { mode_text = { padding = { left = 1, right = 1 } } }, -- add the mode text
-      status.component.git_branch(),
-      status.component.file_info { filetype = {}, filename = false, file_modified = false },
-      status.component.git_diff(),
-      status.component.diagnostics(),
-      status.component.fill(),
-      status.component.cmd_info(),
-      status.component.fill(),
-      status.component.lsp(),
-      status.component.treesitter(),
-      status.component.nav(),
-      -- remove the 2nd mode indicator on the right
-    }
-
-    -- return the final configuration table
+    local status = require "astronvim.utils.status"
+    opts.statusline[3] = status.component.file_info { filetype = {}, filename = false }
+    opts.statusline[1] = status.component.mode { mode_text = { padding = { left = 1, right = 1 } } } -- add the mode text
+    opts.statusline[12] = nil -- remove the 2nd mode indicator on the right
     return opts
   end,
 }
-
-
-
