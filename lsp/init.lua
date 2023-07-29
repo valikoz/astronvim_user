2d086d461d4e26572a9883e0d260b085d986f9f6
master <valikoz1202@gmail.com>
2023-07-29 18:53:28 +0300

    add feature for rust-analyzer

--- lsp/init.lua
+++ lsp/init.lua
@@ -26,6 +26,9 @@ return {
       cmd = {
         "rustup", "run", "stable", "rust-analyzer",
       },
+      flags = {
+        debounce_text_changes = 150,
+      },
     },
   },
   formatting = {
