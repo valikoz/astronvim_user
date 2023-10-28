6e0686a5ae97c49059f11c59b59390c4a15cc03d
sterx <valikoz1202@gmail.com>
2023-10-28 12:40:37 +0300

    remove venv

--- options.lua
+++ options.lua
@@ -5,9 +5,9 @@ return {
   },
   g = {
     -- mapleader = ",",
-    python3_host_prog = vim.fn.has "win64" == 1
-      and '~/AppData/Local/nvim/lua/user/env/Scripts/python'
-      or '~/.config/nvim/lua/user/env/bin/python',
+    -- python3_host_prog = vim.fn.has "win64" == 1
+    --   and '~/AppData/Local/nvim/lua/user/env/Scripts/python'
+    --   or '~/.config/nvim/lua/user/env/bin/python',
     loaded_ruby_provider = 0,
     loaded_perl_provider = 0,
   },
