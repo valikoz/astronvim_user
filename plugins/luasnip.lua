d24b397651823cca5a8a5a1a22eb84d31c4fc2d5
sterx <valikoz1202@gmail.com>
2023-12-12 17:46:09 +0200

    update luasnip settings

--- plugins/luasnip.lua
+++ plugins/luasnip.lua
@@ -6,10 +6,10 @@ return {
       store_selection_keys = "<Tab>",
     }
     -- load snippets paths
-    -- require("luasnip.loaders.from_lua").lazy_load {
-    --   paths = { "./lua/user/snippets" },
-    --   exclude = { vim.fn.has "unix" == 1 and "tex" }
-    -- }
+    require("luasnip.loaders.from_lua").lazy_load {
+      paths = { "./lua/user/snippets" },
+      -- exclude = { vim.fn.has "unix" == 1 and "tex" }
+    }
     require("luasnip.loaders.from_vscode").lazy_load {
       include = { "lua", "python", "rust", "go", "cpp" }
     }
