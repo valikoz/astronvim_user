78635e12a9883792538635740f1a5eeb099610df
sterx <valikoz1202@gmail.com>
2023-12-20 21:34:48 +0200

    remove mapping

--- mappings.lua
+++ mappings.lua
@@ -1,4 +1,4 @@
-local utils = require "user.utils"
+-- local utils = require "user.utils"
 return {
   n = {
       -- disable default bindings
@@ -8,21 +8,22 @@ return {
     ["<C-Up>"] = false,
     ["<C-q>"] = false,
     ["<C-s>"] = false,
-    ["<leader>mp"] = {
-      function()
-        local filename = vim.fn.expand "%:t"
-        if vim.bo.filetype == "python" then
-          vim.cmd "silent! write"
-          utils.async_run({ "python3", vim.fn.expand "%:p" }, {
-            title = "Execute " .. filename
-          })
-        else
-          vim.notify("Cannot execute " .. filename, 3, { title = "Warning" } )
-          return nil
-        end
-      end,
-      desc = "Execute python file",
-    },
+    -- mapping --> command
+    -- ["<leader>mp"] = {
+    --   function()
+    --     local filename = vim.fn.expand "%:t"
+    --     if vim.bo.filetype == "python" then
+    --       vim.cmd "silent! write"
+    --       utils.async_run({ "python3", vim.fn.expand "%:p" }, {
+    --         title = "Execute " .. filename
+    --       })
+    --     else
+    --       vim.notify("Cannot execute " .. filename, 3, { title = "Warning" } )
+    --       return nil
+    --     end
+    --   end,
+    --   desc = "Execute python file",
+    -- },
     -- ["<leader>ml"] = {
     --   function()
     --     local filename = vim.fn.expand "%:t"
