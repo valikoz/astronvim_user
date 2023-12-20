3dfda86686780f5abe3a911bcc728e59150ec5bc
sterx <valikoz1202@gmail.com>
2023-12-20 21:35:26 +0200

    add some commands

--- polish.lua
+++ polish.lua
@@ -1,3 +1,6 @@
+local utils = require "user.utils"
+
+
 -- Remove whitespace
 vim.api.nvim_create_user_command('WhiteSpaceClean', '%s/\\s\\+$//e|nohlsearch', {})
 -- Edit snippets
@@ -10,6 +13,26 @@ vim.api.nvim_create_user_command("LuaSnipInfo", function()
 end, { bang = true, desc = "Open snippets list" })
 -- create an augroup to easily manage autocommands
 vim.api.nvim_create_augroup("autohidetabline", { clear = true })
+-- Execute python files
+vim.api.nvim_create_user_command("ExecutePy", function()
+  local filename = vim.fn.expand "%:t"
+  if vim.bo.filetype == "python" then
+    vim.cmd "silent! write"
+    utils.async_run({ "python3", vim.fn.expand "%:p" }, {
+      title = "Execute " .. filename
+    })
+  else
+    vim.notify("Cannot execute " .. filename, 3, { title = "Warning" } )
+    return nil
+  end
+end,{ bang = true, desc = "Execute python files" })
+-- Start live-server
+vim.api.nvim_create_user_command("Preview", function()
+  -- local filename = vim.fn.expand "%:t"
+  -- os.execute(string.format("live-server %q", vim.fn.expand "%:."))
+  utils.async_run({ "live-server", vim.fn.expand "%:p:h" }, {})
+end,{ bang = true, desc = "Start live-server" })
+
 -- create a new autocmd on the "User" event
 -- vim.api.nvim_create_autocmd("User", {
 --   desc = "Hide tabline when only one buffer and one tab", -- nice description
