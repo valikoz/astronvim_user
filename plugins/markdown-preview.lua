2d2a5a6a571c6e6a45ebbfa4017aecec11214b06
sterx <valikoz1202@gmail.com>
2023-12-20 21:31:30 +0200

    add markdown-preview

---
+++ plugins/markdown-preview.lua
@@ -0,0 +1,11 @@
+-- install without yarn or npm
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function() vim.fn["mkdp#util#install"]() end,
+  config = function() 
+    vim.g.mkdp_theme = 'dark'  -- 'light'
+    -- vim.g.mkdp_filetypes = { 'markdown', }
+  end,
+}
