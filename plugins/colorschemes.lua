71ab7a743fb391018af4d7aa662795654404b83b
Valikoz <valikoz1202@gmail.com>
2024-02-06 15:56:18 +0000

    Add colorscheme

---
+++ plugins/colorschemes.lua
@@ -0,0 +1,20 @@
+return {
+  -- nightfly
+  {
+    "bluz71/vim-nightfly-guicolors",
+  },
+  -- catppuccin
+  {
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.colorscheme.catppuccin" }
+  },
+  -- tokyonight
+  {
+    "folke/tokyonight.nvim",
+    -- opts = {},
+  },
+  -- OceanicNext
+  {
+    "mhartington/oceanic-next",
+  },
+}
