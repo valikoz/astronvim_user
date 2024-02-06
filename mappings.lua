ddc6175f1710f2fdc4f9fd28f74399608edf9a1c
Valikoz <valikoz1202@gmail.com>
2024-02-06 13:56:44 +0000

    Update mappings

--- mappings.lua
+++ mappings.lua
@@ -1,41 +1,12 @@
 -- local utils = require "user.utils"
 return {
   n = {
-      -- disable default bindings
+    -- disable default bindings
     ["<C-Down>"] = false,
     ["<C-Left>"] = false,
     ["<C-Right>"] = false,
     ["<C-Up>"] = false,
     ["<C-q>"] = false,
     ["<C-s>"] = false,
-    -- mapping --> command
-    -- ["<leader>mp"] = {
-    --   function()
-    --     local filename = vim.fn.expand "%:t"
-    --     if vim.bo.filetype == "python" then
-    --       vim.cmd "silent! write"
-    --       utils.async_run({ "python3", vim.fn.expand "%:p" }, {
-    --         title = "Execute " .. filename
-    --       })
-    --     else
-    --       vim.notify("Cannot execute " .. filename, 3, { title = "Warning" } )
-    --       return nil
-    --     end
-    --   end,
-    --   desc = "Execute python file",
-    -- },
-    -- ["<leader>ml"] = {
-    --   function()
-    --     local filename = vim.fn.expand "%:t"
-    --     if vim.bo.filetype == "lua" then
-    --       vim.cmd "silent! write"
-    --       utils.async_run({ "lua", vim.fn.expand "%:p" }, { title = "Execute " .. filename })
-    --     else
-    --       vim.notify("Cannot execute " .. filename, 3, { title = "Warning" } )
-    --       return nil
-    --     end
-    --   end,
-    --   desc = "Execute lua file",
-    -- },
   },
 }
