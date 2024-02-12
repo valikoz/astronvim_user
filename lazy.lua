a9d8b1f741c10563fe4e1ac83fdf4b490447567b
Valikoz <valikoz1202@gmail.com>
2024-02-12 13:58:44 +0000

    Override the default lazy config

---
+++ lazy.lua
@@ -0,0 +1,8 @@
+return {
+  "folke/lazy.nvim",
+    change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = false, -- get a notification when changes are found
+  },
+}
