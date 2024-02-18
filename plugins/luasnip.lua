a239e3aa810aee0787551db42e8144d605d305aa
Red <redschuhart@tutamail.com>
2024-02-18 21:51:16 +0200

    chore(luasnip): add sniplib

--- plugins/luasnip.lua
+++ plugins/luasnip.lua
@@ -1,22 +1,23 @@
 return {
   "L3MON4D3/LuaSnip",
   dependencies = {
-    { "rafamadriz/friendly-snippets", enabled = false, },
-    -- locally load vscode snippets
-    { dir = "~/plugins/friendly-snippets/", name = "vscode-snippets" },
+    -- locally load snippets
+    { dir = "~/plugins/sniplib.nvim", }
   },
   config = function()
     require("luasnip").config.setup {
       enable_autosnippets = true,
       store_selection_keys = "<Tab>",
-      history = false,
+      history = true,
+      update_events = "TextChanged,TextChangedI",
     }
     -- load snippets paths
     require("luasnip.loaders.from_lua").lazy_load {
-      paths = { "./lua/user/snippets/lua", },
+      paths = { "./lua/user/snippets", },
     }
     require("luasnip.loaders.from_vscode").load {
       exclude = { "tex", },
     }
+    require("sniplib").lazy_load()
   end,
 }
