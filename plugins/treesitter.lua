cbf96c44ec2ea80795f163602cd9e4c6579e7aaf
Red <redschuhart@tutamail.com>
2024-02-24 17:51:27 +0200

    chore(treesitter): add treesitter config

---
+++ plugins/treesitter.lua
@@ -0,0 +1,36 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      {
+        "HiPhish/rainbow-delimiters.nvim",
+        opts = function()
+          return {
+            strategy = {
+              [""] = function()
+                if not vim.b.large_buf then return require("rainbow-delimiters").strategy.global end
+              end,
+            },
+          }
+        end,
+        config = function(_, opts) require "rainbow-delimiters.setup"(opts) end,
+      },
+    },
+    opts = {
+      -- auto_install = vim.fn.executable "tree-sitter" == 1,
+
+      context_commentstring = {
+        enable = true,
+
+        -- With Comment.nvim, we don't need to run this on the autocmd.
+        -- Only run it in pre-hook
+        enable_autocmd = false,
+
+        config = {
+          c = "// %s",
+          lua = "-- %s",
+        },
+      },
+    },
+  },
+}
