aceeb34e20eeda19e582424720360f42ad2a8d4e
Red <redschuhart@tutamail.com>
2024-02-24 17:42:48 +0200

    feat(util): add `toggle_qf`

--- utils.lua
+++ utils.lua
@@ -28,6 +28,20 @@ function M.async_run(cmd, opts)
   })
 end
 
+M.toggle_qf = function()
+  local qf_exists = false
+  for _, win in pairs(vim.fn.getwininfo()) do
+    if win["quickfix"] == 1 then
+      qf_exists = true
+      break
+    end
+  end
+  if qf_exists then
+    vim.cmd.cclose()
+  elseif not vim.tbl_isempty(vim.fn.getqflist()) then
+    vim.cmd.copen()
+  end
+end
 
 M.untrigger = function()
   -- get the snippet
