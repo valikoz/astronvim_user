b393130a7541304eb901cdb5e7e3fe247e6ae7e0
Red <redschuhart@tutamail.com>
2024-03-11 15:09:35 +0200

    chore(cmp): change keymaps

--- plugins/cmp.lua
+++ plugins/cmp.lua
@@ -1,3 +1,6 @@
+local utils = require "user.utils"
+
+
 return {
   -- override nvim-cmp plugin
   "hrsh7th/nvim-cmp",
@@ -12,10 +15,6 @@ return {
     local snip_status_ok, luasnip = pcall(require, "luasnip")
     if not snip_status_ok then return end
 
-    local function has_words_before()
-      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
-    end
     -- modify the mapping part of the table
     opts.mapping["\f"] = cmp.mapping { -- <c-l> 0x0C FORM FEED
       i = function()
@@ -27,17 +26,10 @@ return {
       end,
       c = cmp.mapping.close(),
     }
-    -- Use arrows to cycle between nodes from choice_node
-    opts.mapping["<Right>"] = cmp.mapping(function(fallback)
+    -- Cycle between nodes from choice_node
+    opts.mapping["<C-]>"] = cmp.mapping(function(fallback)
       if luasnip.choice_active() then
         luasnip.change_choice(1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' })
-    opts.mapping["<Left>"] = cmp.mapping(function(fallback)
-      if luasnip.choice_active() then
-        luasnip.change_choice(-1)
       else
         fallback()
       end
@@ -50,13 +42,24 @@ return {
       end
     end,
     { 'i', 's' })
-    opts.mapping['<C-n>'] = cmp.config.disable
-    opts.mapping['<C-p>'] = cmp.config.disable
+    opts.mapping['<C-e>'] = cmp.config.disable
+    opts.mapping['<C-n>'] = cmp.mapping(function()
+     if require("luasnip").expand_or_jumpable() then
+      require("luasnip").expand_or_jump()
+     else
+      return nil
+     end
+    end, { 'i', 's' })
+    opts.mapping['<C-p>'] = cmp.mapping(function()
+      if require("luasnip").jumpable() then
+        require("luasnip").jump(-1)
+      else
+        return false
+      end
+    end, { 'i', 's' })
     opts.mapping["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item { behavior = cmp.SelectBehavior.Select }
-      elseif has_words_before() then
-        cmp.complete()
       else
         fallback()
       end
@@ -70,20 +73,31 @@ return {
         fallback()
       end
     end, { "i", "s" })
+    -- Undo a snippet
+    opts.mapping["<c-x>"] = cmp.mapping(function(fallback)
+      if require("luasnip").in_snippet() then
+        utils.untrigger()
+        require("luasnip").unlink_current()
+      else
+        fallback()
+      end
+    end, { "i", "s" })
+    -- Insert otf snippet
+    opts.mapping["<c-f>"] = cmp.mapping {
+      i = function()
+      vim.ui.input({ prompt = 'Enter regname: ' }, function(input)
+        if input == "" or input == nil then return end
+        require('luasnip.extras.otf').on_the_fly(input)
+      end)
+    end,
+    }
     -- modify the sources part of the options table
     opts.sources = cmp.config.sources {
       { name = "nvim_lsp", priority = 1000 },
       { name = "luasnip",  priority = 750 },
-      {
-        name = "buffer",
-        priority = 500,
-        option = {
-          get_bufnrs = function() return vim.api.nvim_list_bufs() end,
-        },
-      },
+      { name = "buffer", priority = 500 },
       { name = "path",     priority = 250 },
       { name = "nvim_lua", priority = 50 },
-      -- { name = "omni",     priority = 50 },
     }
     -- return the new table to be used
     return opts
