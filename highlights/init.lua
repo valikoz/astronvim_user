ffe21b212010976fd5dee16a835e9a3fde0de25e
Red <redschuhart@tutamail.com>
2024-03-11 14:58:39 +0200

    chore(highlights): change highlights settings.

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,10 +1,14 @@
 return function()
   local get_hlgroup = require("astronvim.utils").get_hlgroup
   local nontext = get_hlgroup "NonText"
+  local statement = get_hlgroup "Statement"
   return {
     CursorLineFold = { link = "CursorLineNr" }, -- highlight fold indicator as well as line number
     GitSignsCurrentLineBlame = { fg = nontext.fg, italic = true }, -- italicize git blame virtual text
     HighlightURL = { underline = true }, -- always underline URLs
     OctoEditable = { fg = "NONE", bg = "NONE" }, -- use treesitter for octo.nvim highlighting
+    -- clear the highlight group
+    -- Conceal = {},
+    Conceal = { bold = true, fg = statement.fg },
   }
 end
