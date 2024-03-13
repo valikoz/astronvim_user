b1413f4c06243150014d20d58bbeab7b91f8ab71
Red <redschuhart@tutamail.com>
2024-03-13 21:54:53 +0200

    fix(catppuccin): rm config

--- plugins/catppuccin.lua
+++ plugins/catppuccin.lua
@@ -1,51 +1,8 @@
 return {
   "catppuccin/nvim",
   name = "catppuccin",
-  opts = {
-    transparent_background = false,
-    show_end_of_buffer = true,
-    dim_inactive = { enabled = true, percentage = 0.25 },
-    integrations = {
-      alpha = false,
-      dashboard = false,
-      flash = false,
-      nvimtree = false,
-      ts_rainbow = false,
-      ts_rainbow2 = false,
-      barbecue = false,
-      indent_blankline = false,
-      navic = false,
-      dropbar = false,
-
-      aerial = true,
-      dap = { enabled = true, enable_ui = true },
-      headlines = true,
-      mason = true,
-      native_lsp = { enabled = true, inlay_hints = { background = false } },
-      neogit = true,
-      neotree = true,
-      noice = true,
-      notify = true,
-      sandwich = true,
-      semantic_tokens = true,
-      symbols_outline = true,
-      -- telescope = { enabled = true, style = "nvchad" },
-      which_key = true,
-    },
-    custom_highlights = {
-      -- disable italics  for treesitter highlights
-      TabLineFill = { link = "StatusLine" },
-      LspInlayHint = { style = { "italic" } },
-      ["@parameter"] = { style = {} },
-      ["@type.builtin"] = { style = {} },
-      ["@namespace"] = { style = {} },
-      ["@text.uri"] = { style = { "underline" } },
-      ["@tag.attribute"] = { style = {} },
-      ["@tag.attribute.tsx"] = { style = {} },
-    },
-  },
-  config = function()
-    mappings = {
+  opts = function()
+    local mappings = {
       ["t"] = { function()
         local cat = require("catppuccin")
         cat.options.transparent_background = not cat.options.transparent_background
@@ -59,5 +16,48 @@ return {
       mode = "n",
       prefix = "<leader><localleader>"
     })
-  end
+    return {
+      transparent_background = true,
+      show_end_of_buffer = true,
+      dim_inactive = { enabled = false },
+      integrations = {
+        alpha = false,
+        dashboard = false,
+        flash = false,
+        nvimtree = false,
+        ts_rainbow = false,
+        ts_rainbow2 = false,
+        barbecue = false,
+        indent_blankline = false,
+        navic = false,
+        dropbar = false,
+
+        aerial = true,
+        dap = { enabled = true, enable_ui = true },
+        headlines = true,
+        mason = true,
+        native_lsp = { enabled = true, inlay_hints = { background = false } },
+        neogit = true,
+        neotree = true,
+        noice = true,
+        notify = true,
+        sandwich = true,
+        semantic_tokens = true,
+        symbols_outline = true,
+        which_key = true,
+      },
+      custom_highlights = {
+        -- disable italics  for treesitter highlights
+        TabLineFill = { link = "StatusLine" },
+        LspInlayHint = { style = { "italic" } },
+        ["@parameter"] = { style = {} },
+        ["@type.builtin"] = { style = {} },
+        ["@namespace"] = { style = {} },
+        ["@text.uri"] = { style = { "underline" } },
+        ["@tag.attribute"] = { style = {} },
+        ["@tag.attribute.tsx"] = { style = {} },
+        WhichKeyFloat = { bg = "#1e1e2e" },
+      },
+    }
+  end,
 }
