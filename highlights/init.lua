6702d3c13686b95d8771ea91415ef10def837f5b
Red <redschuhart@tutamail.com>
2024-03-13 21:48:52 +0200

    change telescope & eof highlights

--- highlights/init.lua
+++ highlights/init.lua
@@ -1,14 +1,15 @@
 return function()
   local get_hlgroup = require("astronvim.utils").get_hlgroup
   local nontext = get_hlgroup "NonText"
+  local bg_alt = get_hlgroup("Visual").bg
   local statement = get_hlgroup "Statement"
   return {
     CursorLineFold = { link = "CursorLineNr" }, -- highlight fold indicator as well as line number
     GitSignsCurrentLineBlame = { fg = nontext.fg, italic = true }, -- italicize git blame virtual text
     HighlightURL = { underline = true }, -- always underline URLs
     OctoEditable = { fg = "NONE", bg = "NONE" }, -- use treesitter for octo.nvim highlighting
-    -- clear the highlight group
-    -- Conceal = {},
     Conceal = { bold = true, fg = statement.fg },
+    EndOfBuffer = { link = "NonText" },
+    TelescopeSelection = { fg = "#f2cdcd", bg = bg_alt },
   }
 end
