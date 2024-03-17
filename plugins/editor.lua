8a1b6a7bcd65606121be62c3a87112a48b53bdb0
valikoz <valikoz1202@gmail.com>
2024-03-17 11:54:24 +0200

    chore(editor): add distant plugin

--- plugins/editor.lua
+++ plugins/editor.lua
@@ -132,4 +132,24 @@ return {
      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     }
   },
+  {
+    'chipsenkbeil/distant.nvim', 
+    branch = 'v0.3',
+    cmd = {
+      "DistantLaunch",
+      "DistantOpen",
+      "DistantConnect",
+      "DistantInstall",
+      "DistantMetadata",
+      "DistantShell",
+      "DistantShell",
+      "DistantSystemInfo",
+      "DistantClientVersion",
+      "DistantSessionInfo",
+      "DistantCopy",
+    },
+    config = function()
+        require('distant'):setup()
+    end
+  },
 }
